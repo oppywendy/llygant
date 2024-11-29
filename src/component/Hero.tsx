0316a3c48b0325a8399a4a021b4c56831cee3b25
oppywendy <matrixleooppy60@gmail.com>
2024-11-29 01:46:59 +0100

    go

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -7,6 +7,12 @@ import pic6 from "../assets/ten-removebg-preview.png";
 import pic7 from "../assets/eleven-removebg-preview.png";
 import pic8 from "../assets/fifteen-removebg-preview.png";
 import pic9 from "../assets/twelve-removebg-preview.png";
+import pic10 from "../assets/seventeen-removebg-preview.png";
+import pic11 from "../assets/eighteen-removebg-preview.png";
+import pic12 from "../assets/ninteen-removebg-preview.png";
+import pic13 from "../assets/twenty-removebg-preview.png";
+import pic14 from "../assets/twenty-one-removebg-preview.png";
+import pic15 from "../assets/twenty-two-removebg-preview.png";
 const Hero = () => {
   return (
     <div className="w-full min-h-full justify-items-center items-center grid pb-10">
@@ -117,6 +123,64 @@ const Hero = () => {
             alt=""
           />
           <h1 className="pt-3 md:text-lg text-sm">LLYGANT T-SHIRT (WHITE)</h1>
+          <p className="pt-2 md:text-lg text-sm">$500.00 USD</p>
+        </div>
+        <div className="md:w-[350px] w-[180px] cursor-pointer">
+          <img
+            className="bg-[#E4E4E4] rounded-2xl transition-all duration-300 ease-in-out transform hover:scale-105"
+            src={pic10}
+            alt=""
+          />
+          <h1 className="pt-3 md:text-lg text-sm">LLYGANT T-SHIRT (BLACK)</h1>
+          <p className="pt-2 md:text-lg text-sm">$500.00 USD</p>
+        </div>
+
+        <div className="md:w-[350px] w-[180px] cursor-pointer">
+          <img
+            className="bg-[#E4E4E4] rounded-2xl transition-all duration-300 ease-in-out transform hover:scale-105"
+            src={pic11}
+            alt=""
+          />
+          <h1 className="pt-3 md:text-lg text-sm">LLYGANT T-SHIRT (BLACK)</h1>
+          <p className="pt-2 md:text-lg text-sm">$500.00 USD</p>
+        </div>
+
+        <div className="md:w-[350px] w-[180px] cursor-pointer">
+          <img
+            className="bg-[#E4E4E4] rounded-2xl transition-all duration-300 ease-in-out transform hover:scale-105"
+            src={pic12}
+            alt=""
+          />
+          <h1 className="pt-3 md:text-lg text-sm">LLYGANT T-SHIRT (BLACK)</h1>
+          <p className="pt-2 md:text-lg text-sm">$500.00 USD</p>
+        </div>
+        <div className="md:w-[350px] w-[180px] cursor-pointer">
+          <img
+            className="bg-[#E4E4E4] rounded-2xl transition-all duration-300 ease-in-out transform hover:scale-105"
+            src={pic13}
+            alt=""
+          />
+          <h1 className="pt-3 md:text-lg text-sm">LLYGANT T-SHIRT (BLACK)</h1>
+          <p className="pt-2 md:text-lg text-sm">$500.00 USD</p>
+        </div>
+
+        <div className="md:w-[350px] w-[180px] cursor-pointer">
+          <img
+            className="bg-[#E4E4E4] rounded-2xl transition-all duration-300 ease-in-out transform hover:scale-105"
+            src={pic14}
+            alt=""
+          />
+          <h1 className="pt-3 md:text-lg text-sm">LLYGANT T-SHIRT (BLACK)</h1>
+          <p className="pt-2 md:text-lg text-sm">$500.00 USD</p>
+        </div>
+
+        <div className="md:w-[350px] w-[180px] cursor-pointer">
+          <img
+            className="bg-[#E4E4E4] rounded-2xl transition-all duration-300 ease-in-out transform hover:scale-105"
+            src={pic15}
+            alt=""
+          />
+          <h1 className="pt-3 md:text-lg text-sm">LLYGANT T-SHIRT (BLACK)</h1>
           <p className="pt-2 md:text-lg text-sm">$500.00 USD</p>
         </div>
       </div>
